444e9350b32b3510d80fc4aa8caa40e6b75ef8d8
Jakob Nilsson <jakob.a.nilsson@gmail.com>
2019-08-08 16:19:57 +0200

    Check site status

---
+++ Egna exempel/esp8266-nodemcu/Secure_client_led/Secure_client_led/Secure_client_led.ino
@@ -0,0 +1,85 @@
+/**
+   BasicHTTPSClient.ino
+
+    Created on: 20.08.2018
+
+*/
+
+#include <Arduino.h>
+
+#include <ESP8266WiFi.h>
+#include <ESP8266WiFiMulti.h>
+
+#include <ESP8266HTTPClient.h>
+
+#include <WiFiClientSecureBearSSL.h>
+const char fingerprint[] PROGMEM = "992abe29ac9288b4521e7aee38d0e4d2191cb187"; //Valid to ‎25 ‎April ‎2020 01:59:59
+
+ESP8266WiFiMulti WiFiMulti;
+
+void setup() {
+  pinMode(D0, OUTPUT);     // Initialize the LED
+     digitalWrite(D0, HIGH);  // Turn the LED off
+  Serial.begin(115200);
+  // Serial.setDebugOutput(true);
+
+  Serial.println();
+  Serial.println();
+  Serial.println();
+
+  for (uint8_t t = 4; t > 0; t--) {
+    Serial.printf("[SETUP] WAIT %d...\n", t);
+    Serial.flush();
+    delay(1000);
+  }
+
+  WiFi.mode(WIFI_STA);
+  WiFiMulti.addAP("Nissen", "bullerbysyndrom");
+}
+
+void loop() {
+  // wait for WiFi connection
+  if ((WiFiMulti.run() == WL_CONNECTED)) {
+
+    std::unique_ptr<BearSSL::WiFiClientSecure>client(new BearSSL::WiFiClientSecure);
+
+    client->setFingerprint(fingerprint);
+
+    HTTPClient https;
+
+    Serial.print("[HTTPS] begin...\n");
+    if (https.begin(*client, "https://kuben.vgs.no/")) {  // HTTPS
+
+      Serial.print("[HTTPS] GET...\n");
+      // start connection and send HTTP header
+      int httpCode = https.GET();
+
+      // httpCode will be negative on error
+      if (httpCode > 0) {
+        // HTTP header has been send and Server response header has been handled
+        Serial.printf("[HTTPS] GET... code: %d\n", httpCode);
+
+if (httpCode == 200){ //If site loads correctly
+   digitalWrite(D0, LOW);  // Turn the LED on
+} else {
+   digitalWrite(D0, HIGH);  // Turn the LED off
+}
+
+        // file found at server
+        if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY) {
+          String payload = https.getString();
+          Serial.println(payload);
+        }
+      } else {
+        Serial.printf("[HTTPS] GET... failed, error: %s\n", https.errorToString(httpCode).c_str());
+      }
+
+      https.end();
+    } else {
+      Serial.printf("[HTTPS] Unable to connect\n");
+    }
+  }
+
+  Serial.println("Wait 10s before next round...");
+  delay(10000);
+}
